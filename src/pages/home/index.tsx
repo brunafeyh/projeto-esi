e2c8df78c5a870290b160d2e33e0d7eb6e0baca7
Bruna Feyh <bruna.feyh@unioeste.br>
2024-08-19 15:43:57 -0300

    feat: implementing image card

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
 import { PageLayout } from '../../layouts/page-layout'
 import { TitlePage } from './styles'
 import { FC } from 'react'
@@ -8,6 +9,26 @@ const HomePage: FC = () => {
 			<TitlePage>Os mais vendidos</TitlePage>
 
 			<TitlePage>Recomendação da cozinha</TitlePage>
+			<Card sx={{ maxWidth: 345 }}>
+				<CardMedia
+					sx={{ height: 140 }}
+					image="/images/logo-restaurante-clara.png"
+					title="green iguana"
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="div">
+						Lizard
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						Lizards are a widespread group of squamate reptiles, with over 6,000
+						species, ranging across all continents except Antarctica
+					</Typography>
+				</CardContent>
+				<CardActions>
+					<Button size="small">Share</Button>
+					<Button size="small">Learn More</Button>
+				</CardActions>
+			</Card>
 		</PageLayout>
 	)
 }
