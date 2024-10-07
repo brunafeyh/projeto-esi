640747d6b639e36fe1e56f4e48e5857f2c92e9bc
Bruna Feyh <bruna.sf@itaipuparquetec.org.br>
2024-10-07 17:27:38 -0300

    fix: responsiveness on the login page

--- src/components/login-header/index.tsx
+++ src/components/login-header/index.tsx
@@ -1,17 +1,16 @@
-import { PresentationContainer, Title, Typography } from './styles'
+import { PresentationContainer, Title, Typography } from './styles';
 
 const LoginHeader = () => {
-	return (
-		<PresentationContainer style={{ position: 'relative', display: 'inline-block' }}>
-			<Title variant="h4"> Receba carinho em forma de sabor! </Title>
-			<Typography variant="h4"> ~ Restaurante carinho </Typography>
-			<img
-				src="/images/imagem-login.png"
-				alt="Imagem de prato"
-				style={{ width: '740px', height: '738px' }}
-			/>
-		</PresentationContainer>
-	)
+    return (
+        <PresentationContainer style={{ position: 'relative', display: 'inline-block' }}>
+            <Title variant="h4"> Receba carinho em forma de sabor! </Title>
+            <Typography variant="h4"> ~ Restaurante carinho </Typography>
+                <img
+                    src="/images/imagem-login.png"
+                    alt="Imagem de prato"
+                    style={{ width: '100%', height: '100%' }}
+                />
+        </PresentationContainer>
+    )
 }
-
 export default LoginHeader
